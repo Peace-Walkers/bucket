b653807ca43bfe7f7fe1af497c515bd30c5fc4f0
pepedinho <2spii94@gmail.com>
2026-01-08 21:41:28 +0100

    core/storage/test: add additional test for multiple groups

--- src/core/storage.rs
+++ src/core/storage.rs
@@ -64,4 +64,42 @@ mod storage_loading {
         assert_eq!(g.notes()[0].name(), "borrowing.bck");
         Ok(())
     }
+
+    #[test]
+    fn test_load_multiple_group() -> anyhow::Result<()> {
+        let dir = tempdir()?;
+        let a_group_dir = dir.path().join("go"); // group
+        let b_group_dir = dir.path().join("python"); // group
+        let c_group_dir = dir.path().join("rust"); // group
+
+        fs::create_dir(&a_group_dir)?;
+        fs::create_dir(&b_group_dir)?;
+        fs::create_dir(&c_group_dir)?;
+
+        let a_note_path = a_group_dir.join("borrowing.bck");
+        let b_note_path = b_group_dir.join("borrowing.bck");
+        let c_note_path = c_group_dir.join("borrowing.bck");
+
+        File::create(&a_note_path)?;
+        File::create(&b_note_path)?;
+        File::create(&c_note_path)?;
+
+        let mut groups = Storage::load_groups(dir.path())?;
+        groups.sort_by(|a, b| a.name().cmp(b.name())); // sort by name
+
+        assert_eq!(groups.len(), 3);
+        let a = &groups[0];
+        assert_eq!(a.notes().len(), 1);
+        assert_eq!(a.name(), "go");
+        assert_eq!(a.notes()[0].name(), "borrowing.bck");
+        let b = &groups[1];
+        assert_eq!(b.notes().len(), 1);
+        assert_eq!(b.name(), "python");
+        assert_eq!(b.notes()[0].name(), "borrowing.bck");
+        let c = &groups[2];
+        assert_eq!(c.notes().len(), 1);
+        assert_eq!(c.name(), "rust");
+        assert_eq!(c.notes()[0].name(), "borrowing.bck");
+        Ok(())
+    }
 }
